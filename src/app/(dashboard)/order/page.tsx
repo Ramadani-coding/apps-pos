8e869a54413d529fcddbf829a6f9ecb79e50e933
Ramadani-coding <rama.id2210@icloud.com>
2025-08-24 21:55:28 +0800

    update: create order

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,9 @@
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+  title: "Rama Cafe | Order",
+};
+
+export default function UserManagementPage() {
+  return <OrderManagement />;
+}
