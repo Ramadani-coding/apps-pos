b49d084abdf07236e83137266eab397d4496da6f
Ramadani-coding <rama.id2210@icloud.com>
2025-08-24 18:32:39 +0800

    update: table list

---
+++ src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,9 @@
+import TableManagement from "./_components/table";
+
+export const metadata = {
+  title: "Rama Cafe | Menu Management",
+};
+
+export default function MenuManagementPage() {
+  return <TableManagement />;
+}
--- src/migrations/003-tables.sql
+++ src/migrations/003-tables.sql
@@ -1,16 +1,13 @@
-create table public.menus (
+create table public.tables (
   id serial not null,
   name text,
   description text,
-  price numeric,
-  discount numeric,
-  image_url text,
-  category text,
-  is_available boolean,
+  capacity numeric,
+  status text,
   created_at timestamp with time zone default timezone('utc'::text, now()) not null,
   updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
 
   primary key (id)
 );
 
-alter table public.menus enable row level security;
+alter table public.tables enable row level security;
